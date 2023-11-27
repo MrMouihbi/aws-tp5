d3ecfc61c280c61ecca1826219c97018a843b7da
mrmouihbi <mohamadmouihbi@gmail.com>
2023-11-27 11:43:15 +0100

    initial commit

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = 
+"postgresql://postgres:password@database-2.co5sxdx6svcl.us-east-1.rds.amazonaws.com/postgres"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
